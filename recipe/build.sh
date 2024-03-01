5d6c77e5698dea03c8b739c4e542808706d083f3
Prayag Singh <prayags@amazon.com>
2024-03-01 10:22:43 -0800

    Alternate implementation for using conda ripgrep

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,13 +11,16 @@ pushd src
 # Install node-gyp globally as a fix for NodeJS 18.18.2 https://github.com/microsoft/vscode/issues/194665
 npm i -g node-gyp
 
-# fetch the version of ripgrep, removes a ^ if present
-VSCODE_RIPGREP_VERSION=$(jq -r '.dependencies."@vscode/ripgrep" | sub("^\\^"; "")' package.json)
-# ripgrep tries to download binary from github, copying the ripgrep executable to the cache location so that it doesn't do that 
-mkdir /tmp/vscode-ripgrep-cache-${VSCODE_RIPGREP_VERSION}
-cp "${PREFIX}/bin/rg" /tmp/vscode-ripgrep-cache-${VSCODE_RIPGREP_VERSION}/
+# Install all dependencies except @vscode/ripgrep
+VSCODE_RIPGREP_VERSION=$(jq -r '.dependencies."@vscode/ripgrep"' package.json)
+mv package.json package.json.orig
+jq 'del(.dependencies."@vscode/ripgrep")' package.json.orig > package.json
 
 yarn install
+
+# Install @vscode/ripgrep without downloading the pre-built ripgrep.
+# This often runs into Github API ratelimits and we won't use the binary in this package anyways.
+yarn add --ignore-scripts "@vscode/ripgrep@${VSCODE_RIPGREP_VERSION}"
 
 ARCH_ALIAS=linux-x64
 yarn gulp vscode-reh-web-${ARCH_ALIAS}-min
@@ -44,3 +47,11 @@ PREFIX_DIR=$(dirname ${PREFIX_DIR})
 node "${PREFIX_DIR}/share/sagemaker-code-editor/out/server-main.js" "$@"
 EOF
 chmod +x ${PREFIX}/bin/sagemaker-code-editor
+
+# Replace node ripgrep with conda package
+mkdir -p ${PREFIX}/share/sagemaker-code-editor/node_modules/@vscode/ripgrep/bin
+cat <<EOF >${PREFIX}/share/sagemaker-code-editor/node_modules/@vscode/ripgrep/bin/rg
+#!/bin/bash
+exec "${PREFIX}/bin/rg" "\$@"
+EOF
+chmod +x ${PREFIX}/share/sagemaker-code-editor/node_modules/@vscode/ripgrep/bin/rg
